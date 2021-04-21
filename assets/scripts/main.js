cec2f39c0651685ac2ba4b2385347fd8fedd609f
Sam Sweigart <samuel.sweigart@gmail.com>
2021-04-20 20:42:31 -0400

    refactor to add jquery / bootstrap / projects

---
+++ assets/scripts/main.js
@@ -0,0 +1,20 @@
+console.log('Welcome to my portfolio page. Stop back soon for an update!')
+const welcomeContainer = $('#welcome-container')
+const greetUser = $('#greet-user')
+const welcomeUser = $('#welcome-user')
+const heart = $('#welcome-heart');
+// Sidebar Nav fade in just for fun
+// Will expand to full page fade in
+$(document).ready(function() {
+    welcomeContainer.fadeOut().fadeIn(3000);
+
+})
+
+ heart.on('click', function() {
+    welcomeContainer.empty();
+    
+    /* STARTING POINT 4/21/21 AM */
+
+    // add remove class .hide to... 
+    // main page & create main page html/css
+})
--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-console.log('Welcome to my portfolio page. Stop back soon for an update!')
-
-// Sidebar Nav fade in just for fun
-// Will expand to full page fade in
-
-$(window).ready(function(){
-      $("#fadeIn").fadeIn(2000);
-  });
-
