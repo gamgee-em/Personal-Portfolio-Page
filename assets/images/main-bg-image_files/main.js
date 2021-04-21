f5167bd06ff03e4031ce28f1bb9dda535b9cafb8
Sam Sweigart <samuel.sweigart@gmail.com>
2021-04-21 08:29:53 -0400

    main page fades in nicely now / added rough sidenav

--- assets/images/main-bg-image_files/main.js
+++ assets/images/main-bg-image_files/main.js
@@ -4,7 +4,7 @@ const greetUser = $('#greet-user');
 const welcomeUser = $('#welcome-user');
 const heart = $('#welcome-heart');
 const mainContain = $('#main-container');
-const sideNav = $('#side-nav');
+
 // on LOAD page 'fades out' instantly to create...
 // fadeIn effect / look into a smoother way to start?
 $(document).ready(function() {
@@ -13,24 +13,21 @@ $(document).ready(function() {
 })
 
 // heart icon on CLICK fades out welcome page... 
-// & adds hide class to then display main page bg-img
+// and displays main page
 heart.on('click', function() {
     welcomeContain.fadeOut(3000, function() {
         welcomeContain.attr({
             style: 'background-color: white',
             class: 'hide'
         });
+
     }).empty();
 
     // TODO STARTING POINT 4/21/21 AM 
-    // adds background bg-img to MAIN container when...
+    // adds background bg-image to MAIN container when...
     // heart is clicked
-    // TODO switch for local img do not rely on external links if poss
     mainContain.css('background', 'url(https://www.w3schools.com/howto/img_parallax.jpg) no-repeat center');
-    sideNav.css({
-        color: 'black',
-        background: 'gray',
-        height: '100%',
-        width: '20vw'
-    })
+
+    
+
 })
