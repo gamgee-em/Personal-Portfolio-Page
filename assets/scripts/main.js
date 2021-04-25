7d9ccd2a001e036f8d282e41bef5d874aa455607
Sam Sweigart <samuel.sweigart@gmail.com>
2021-04-25 19:57:41 -0400

    resized thumbnails to be same size

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,7 +21,6 @@ const aboutContain = $('#about-container')
 const resumeIcon = $('#resume-icon');
 // on LOAD page 'fades out' instantly to create...
 // fadeIn effect / look into a smoother way to start?
-// TODO: for loop over welcome string and fade in each index (letter)
 welcomeContain.fadeOut(500);
 welcomeContain.fadeIn(1000);
 closeNav.hide();
@@ -50,22 +49,26 @@ const getGitData = async () => {
 const mainRender = ()=> {
     welcomeContain.fadeOut(1000, ()=> {
         // TODO: try using the jquery method .hide('speed value') instead 
-        welcomeContain.attr({
-            style: 'background-color: white',
-            class: 'hide'
-        });
+        welcomeContain.attr('class', 'hide');
     }).empty();
 
     // adds background bg-img to MAIN container when...
     // heart is clicked
     // TODO: switch for local img do not rely on external links if poss
-    mainContain.css('background', 'url(https://www.w3schools.com/howto/img_parallax.jpg) 100% no-repeat');
+    mainContain.css({
+        'background': 'url(https://www.w3schools.com/howto/img_parallax.jpg)',
+        'background-repeat': 'no-repeat',
+        'background-size': 'cover',
+        'background-position': 'center',
+        'font-size': '1.5vw'
+    });
+
     sideNav.css({
         color: 'black',
         background: 'linear-gradient(135deg,rgb(176, 187, 199) 40%,rgb(128, 128, 128) 40%)',
         height: '100vh',
         width: '20vw',
-        padding: '5% 0 25% 0'
+        padding: '5% 0 15% 0'
     }).show(1000);
     
     icons.fadeIn(1000);
@@ -85,6 +88,20 @@ const slideNavRender = () => {
     closeNav.show(1000);
 };
 
+const closeNavRender = () => {
+    closeNav.hide(1000);
+    portCardsContainer.fadeOut(500);
+    icons.show(1000)
+    resumeIcon.hide(1000)
+    socialIcons.show(1000)
+    fileIcons.hide(1000)
+    aboutContain.fadeOut(500);
+    // show ul li elements
+    sideNav.children().show(1000)
+    sideNav.css('width', '20vw')
+    icons.css('width', '25vw');
+}
+
     /* CLICK EVENTS */
 
 // heart icon on CLICK fades out welcome page... 
@@ -100,11 +117,9 @@ aboutNav.on('click',() => {
 });
 
     /* RESUME */
-// TODO: add screenshot of resume & clickable/downloadable pdf
 resumeNav.on('click',() => {
     resumeIcon.show(1000);
     slideNavRender();
-
 });
 
     /* PORTFOLIO */
@@ -120,16 +135,5 @@ contactNav.on()
     /* CLOSE SLIDERS */
 // return to main page when clicked
 closeNav.on('click', () => {
-    closeNav.hide(1000);
-    portCardsContainer.fadeOut(500);
-    icons.show(1000)
-    resumeIcon.hide(1000)
-    socialIcons.show(1000)
-    fileIcons.hide(1000)
-    aboutContain.fadeOut(500);
-    // show ul li elements
-    sideNav.children().show(1000)
-    sideNav.css('width', '20vw')
-    icons.css('width', '25vw');
-
+    closeNavRender();
 })
